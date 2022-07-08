659ca1c7a0b18f0926b833ba818cebf6563451e1
unknown <diegoreis.dvlopr@gmail.com>
2022-07-08 01:53:09 -0300

    exercício 3 finalizado

---
+++ src/exercicio3.ts
@@ -0,0 +1,29 @@
+let botaoAtualizar = document.getElementById('atualizar-saldo') as HTMLElement;
+let botaoLimpar = document.getElementById('limpar-saldo') as HTMLElement;
+let soma = document.getElementById('soma') as HTMLInputElement;
+let campoSaldo = document.getElementById('campo-saldo') as HTMLInputElement;
+
+campoSaldo.innerHTML = '0' ;
+
+function somarAoSaldo() {
+    campoSaldo.innerHTML += soma;
+}
+
+function limparSaldo() {
+    campoSaldo.innerHTML = '';
+}
+
+botaoAtualizar.addEventListener('click', function () {
+    somarAoSaldo(Number(soma.value));
+});
+
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
+
+/**
+    <h4>Valor a ser adicionado: <input id="soma"> </h4>
+    <button id="atualizar-saldo">Atualizar saldo</button>
+    <button id="limpar-saldo">Limpar seu saldo</button>
+    <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
+ */
